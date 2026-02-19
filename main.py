ce4d280696aa71ec2cf3086110bc8ad1454f1e74
ShivangSrivastava <shivangsrivastava157@gmail.com>
2026-02-19 13:07:05 +0530

    add classifier model and dataset for poses

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import sys
 
 from src.collect_data import start_collection
+from src.realtime import classify
 
 
 def main():
     print("=== AI Yoga Assist ===")
     print("1. Start Data Collection")
+    print("2. Start Classification")
     print("q. Quit")
 
     choice = input("\nSelect an option: ")
@@ -13,6 +15,9 @@ def main():
     if choice == "1":
         print("\nLaunching Data Collector...")
         start_collection(csv_path="data/poses.csv")
+    elif choice == "2":
+        print("\nLaunching Classification...")
+        classify("./models/pose_classifier.h5","./models/label_encoder.pkl")
     elif choice.lower() == "q":
         print("Goodbye!")
         sys.exit()
